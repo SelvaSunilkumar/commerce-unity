6e85af8c4b0a841cfdd97e21bc152523124f8ec5
S Sunil Kumar <v-sunil.s@turvo.com>
2025-08-22 18:15:25 +0530

    Fixing the errors with Correlation

--- src/main/java/com/github/SelvaSunilkumar/Commerce/Unity/correlation/CorrelationIdFilter.java
+++ src/main/java/com/github/SelvaSunilkumar/Commerce/Unity/correlation/CorrelationIdFilter.java
@@ -2,16 +2,18 @@ package com.github.SelvaSunilkumar.Commerce.Unity.correlation;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.UUID;
 
 @Component
-public class CorrelationIdFilter implements Filter {
+public class CorrelationIdFilter extends OncePerRequestFilter {
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+    protected void doFilterInternal(HttpServletRequest servletRequest, HttpServletResponse servletResponse, FilterChain filterChain) throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
         String correlationId = request.getHeader(CorrelationIdConstants.HEADER_NAME);
 
@@ -20,7 +22,9 @@ public class CorrelationIdFilter implements Filter {
         }
 
         CorrelationIdHolder.setCorrelationId(correlationId);
+        request.setAttribute(CorrelationIdConstants.HEADER_NAME, correlationId);
         MDC.put(CorrelationIdConstants.HEADER_NAME, correlationId);
+        logger.info(String.format("setting correlation Id: %s", correlationId));
 
         try {
             filterChain.doFilter(request, servletResponse);
